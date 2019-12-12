e8d6a48a229ee33462a049be2cc459a06abe3743
Sonali Aggarwal <sonali.agg02@gmail.com>
2019-12-12 18:56:03 -0500

    solved indian job problem using DP

---
+++ src/IndianJob.java
@@ -0,0 +1,44 @@
+public class IndianJob {
+
+    private static int[][] dp = new int[100][10001];
+    private static int[] tt = new int[101];
+
+    static String indianJob(int g, int[] arr) {
+        int n = arr.length;
+        int sum = 0;
+        dp[0][0] = 1;
+
+        for(int i = 0; i < n; i++){
+            tt[i] = arr[i];
+            sum += tt[i];
+        }
+
+        for(int j = 0; j < 10001; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i = 1; i <= n; i ++){
+            System.arraycopy(dp[i - 1], 0, dp[i], 0, 10001);
+            for(int j = 0; j < 10001; j ++){
+                if(j - tt[i] >= 0 ) {
+                    dp[i][j] |= dp[i - 1][j - tt[i]];
+                }
+            }
+        }
+        boolean result = false;
+        int j = 0;
+        while(j <= sum){
+            if(j <= g && dp[n][j] == 1 && sum - j <= g){
+                result = true;
+                break;
+            }
+            j++;
+        }
+
+        if(result){
+            return "YES";
+        }else{
+            return "NO";
+        }
+    }
+}
